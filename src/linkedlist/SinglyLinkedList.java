9f5d91974b968f35416530522c59173e42547180
jchoi <jchoi@Hacker>
2019-05-03 13:40:59 -0400

    singly linked list

---
+++ src/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class SinglyLinkedList {
+
+	class Node {
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
